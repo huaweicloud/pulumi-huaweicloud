4a77c64945086ef0d3d38575f135b4af8854e3eb
Jason Zhang <jasonzhang9309@163.com>
2023-02-20 17:00:04 +0800

    update scripts

--- scripts/config_files.sh
+++ scripts/config_files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sed -i 's+github.com/pulumi/registry+github.com/Jason-Zhang9309/registry+g' docs/config/_default/config.yml
-sed -i 's+https://github.com/pulumi/registry+https://github.com/Jason-Zhang9309/registry+g' docs/tools/resourcedocsgen/cmd/docs/docs.go
+sed -i 's+github.com/pulumi/registry+github.com/chnsz/registry+g' docs/config/_default/config.yml
+sed -i 's+https://github.com/pulumi/registry+https://github.com/chnsz/registry+g' docs/tools/resourcedocsgen/cmd/docs/docs.go
 sed -i '26,28d' docs/scripts/build-site.sh
 sed -i 's+go build -o "${GOPATH}/bin/resourcedocsgen" .+go build .+g' docs/scripts/build-site.sh
 sed -i 's+resourcedocsgen docs registry --commitSha "${REGISTRY_COMMIT}" --logtostderr+./resourcedocsgen docs registry --logtostderr+g' docs/scripts/build-site.sh
